d4b38c6356e46ddf6dfa01d5593dff918646a412
rong <211103376@qq.com>
2025-01-16 04:27:29 +0800

    simple text segment test for string

--- modules/core/base/src/SkrBase/build.string.cpp
+++ modules/core/base/src/SkrBase/build.string.cpp
@@ -7,6 +7,10 @@
     #include <psapi.h>
     #include <processthreadsapi.h>
     #include <libloaderapi.h>
+#elif SKR_PLAT_MACOSX
+    #include <mach-o/getsect.h>
+    #include <mach-o/dyld.h>
+    #include <dlfcn.h>
 #elif !SKR_PLAT_MACOSX && SKR_PLAT_UNIX
 extern char etext, edata, end;
 #endif
@@ -22,6 +26,9 @@ bool in_const_segment(const void* ptr)
     SKR_ASSERT(b && "Failed to get module information");
     return uintptr_t(ptr) > uintptr_t(module_info.EntryPoint) &&
            uintptr_t(ptr) < (uintptr_t(module_info.EntryPoint) + module_info.SizeOfImage);
+#elif SKR_PLAT_MACOSX
+    static const struct segment_command_64 *segment = getsegbyname("__TEXT");
+    return (ptr > (const void*)segment->vmaddr) && (ptr < (const void*)(segment->vmaddr + segment->vmsize));
 #elif !SKR_PLAT_MACOSX && SKR_PLAT_UNIX
     return (ptr > &etext) && (ptr < &edata);
 #else
