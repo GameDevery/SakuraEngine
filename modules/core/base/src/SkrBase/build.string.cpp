fd5218ab039b42ba7e37231f6fcbb11d4a07c483
rong <211103376@qq.com>
2025-01-16 03:14:46 +0800

    fix const segment test for macos

--- modules/core/base/src/SkrBase/build.string.cpp
+++ modules/core/base/src/SkrBase/build.string.cpp
@@ -1,12 +1,13 @@
 #include "SkrBase/containers/string/literal.hpp"
 #include "SkrBase/misc/debug.h"
+#include <cstdlib>
 
 // platform specific imports
 #if SKR_PLAT_WINDOWS
     #include <psapi.h>
     #include <processthreadsapi.h>
     #include <libloaderapi.h>
-#elif SKR_PLAT_UNIX
+#elif !SKR_PLAT_MACOSX && SKR_PLAT_UNIX
 extern char etext, edata, end;
 #endif
 
@@ -21,7 +22,7 @@ bool in_const_segment(const void* ptr)
     SKR_ASSERT(b && "Failed to get module information");
     return uintptr_t(ptr) > uintptr_t(module_info.EntryPoint) &&
            uintptr_t(ptr) < (uintptr_t(module_info.EntryPoint) + module_info.SizeOfImage);
-#elif SKR_PLAT_UNIX
+#elif !SKR_PLAT_MACOSX && SKR_PLAT_UNIX
     return (ptr > &etext) && (ptr < &edata);
 #else
     static const char*     test_str              = "__A UNIQUE CONST SEGMENT STRING__";
