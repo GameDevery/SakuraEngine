92c2b314b2fef4574112b57052848e2cdc5b3d87
rong <211103376@qq.com>
2025-01-16 15:21:28 +0800

    use SDL2 prebuilt package

--- thirdparty/SDL2/xmake.lua
+++ thirdparty/SDL2/xmake.lua
@@ -2,19 +2,14 @@ target("SDL2")
     set_kind("headeronly")
 
     -- add packages
-    if (is_os("windows")) then
-        -- use embedded header
-        add_includedirs("include", {public = true})
-        
-        -- download prebuilt binaries
-        skr_install_rule()
-        skr_install("download", {
-            name = "SDL2",
-            install_func = "sdk",
-        })
+    add_includedirs("include", {public = true})
+    
+    -- download prebuilt binaries
+    skr_install_rule()
+    skr_install("download", {
+        name = "SDL2",
+        install_func = "sdk",
+    })
 
-        -- add links
-        add_links("SDL2", {public = true})
-    elseif (is_os("macosx") or is_os("linux")) then
-        add_packages("libsdl", {public = true})
-    end
+    -- add links
+    add_links("SDL2", {public = true})
