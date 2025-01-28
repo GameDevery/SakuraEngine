b60df769c6a52522d38093e4dfcd60189c178bd2
rong <211103376@qq.com>
2025-01-28 15:17:37 +0800

    remove input lib

--- modules/engine/input/xmake.lua
+++ modules/engine/input/xmake.lua
@@ -5,12 +5,14 @@ shared_module("SkrInput", "SKR_INPUT")
     if (is_plat("windows")) then
         add_defines("SKR_INPUT_USE_GAME_INPUT", {public=false})
         add_files("src/game_input/**.cpp")
-        add_linkdirs("lib/x64", {public=true})
-        if (is_mode("release")) then
-            add_linkdirs("lib/x64/Release", {public=true})
-        else
-            add_linkdirs("lib/x64/Debug", {public=true})
-        end
+
+        -- input links
+        -- add_linkdirs("lib/x64", {public=true})
+        -- if (is_mode("release")) then
+        --     add_linkdirs("lib/x64/Release", {public=true})
+        -- else
+        --     add_linkdirs("lib/x64/Debug", {public=true})
+        -- end
         -- add_links("GameInput", "xgameruntime", "Microsoft.Xbox.Services.141.GDK.C.Thunks", {public=false})
     end
     add_files("src/*.cpp")
