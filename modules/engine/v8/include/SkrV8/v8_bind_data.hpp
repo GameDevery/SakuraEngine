6e8c03ef830ff94d667077c252eed8700ef5a1c6
rong <211103376@qq.com>
2025-03-03 15:42:03 +0800

    better v8 bind data

--- modules/engine/v8/include/SkrV8/v8_bind_data.hpp
+++ modules/engine/v8/include/SkrV8/v8_bind_data.hpp
@@ -5,6 +5,56 @@
 
 namespace skr::v8
 {
+struct V8BindRecordCore;
+
+struct V8BindMethodCore {
+    struct OverloadInfo {
+        skr::rttr::Type* type;
+        skr::rttr::MethodData* method;
+    };
+
+    // native info
+    String name;
+    Vector<OverloadInfo> overloads;
+
+    // owner record bind core
+    V8BindRecordCore* owner;
+};
+
+struct V8BindStaticMethodCore {
+    struct OverloadInfo {
+        skr::rttr::Type* type;
+        skr::rttr::StaticMethodData* method;
+    };
+
+    // native info
+    String name;
+    Vector<OverloadInfo> overloads;
+
+    // owner record bind core
+    V8BindRecordCore* owner;
+};
+
+struct V8BindFieldCore {
+    // native info
+    String name;
+    skr::rttr::Type* type;
+    skr::rttr::FieldData* field;
+
+    // owner record bind core
+    V8BindRecordCore* owner;
+};
+
+struct V8BindStaticFieldCore {
+    // native info
+    String name;
+    skr::rttr::Type* type;
+    skr::rttr::StaticFieldData* field;
+
+    // owner record bind core
+    V8BindRecordCore* owner;
+};
+
 struct V8BindRecordCore {
     // native info
     skr::rttr::ScriptbleObject* object;
@@ -12,5 +62,13 @@ struct V8BindRecordCore {
 
     // v8 info
     ::v8::Persistent<::v8::Object> v8_object;
+
+    // field & method
+    Map<String, V8BindMethodCore*> methods;
+    Map<String, V8BindFieldCore*>  fields;
+
+    // static field & static method
+    Map<String, V8BindStaticMethodCore*> static_methods;
+    Map<String, V8BindStaticFieldCore*>  static_fields;
 };
 } // namespace skr::v8
