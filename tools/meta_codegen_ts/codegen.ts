19dfa4c6025b81e974f9c67e0f061613ec1c4c88
rong <211103376@qq.com>
2025-03-10 01:09:43 +0800

    better meta lang

--- tools/meta_codegen_ts/codegen.ts
+++ tools/meta_codegen_ts/codegen.ts
@@ -25,7 +25,7 @@ const codegen_config = new config.CodegenConfig(
 const proj_db = new db.Project(codegen_config);
 
 // load meta lang parser
-const parser = ml.load_parser();
+const parser = ml.Compiler.load();
 
 // compile attrs exprs
 // proj_db.each_cpp_types((cpp_type, _header) => {
@@ -38,15 +38,91 @@ const parser = ml.load_parser();
 // combine attrs context
 
 // run attrs expr
-proj_db.each_cpp_types((cpp_type, _header) => {
-  if (cpp_type instanceof cpp.Enum) {
-  } else if (cpp_type instanceof cpp.EnumValue) {
-  } else if (cpp_type instanceof cpp.Record) {
-  } else if (cpp_type instanceof cpp.Field) {
-  } else if (cpp_type instanceof cpp.Method) {
-  } else if (cpp_type instanceof cpp.Function) {
-  } else if (cpp_type instanceof cpp.Parameter) {
-  }
-});
+// proj_db.each_cpp_types((cpp_type, _header) => {
+//   if (cpp_type instanceof cpp.Enum) {
+//   } else if (cpp_type instanceof cpp.EnumValue) {
+//   } else if (cpp_type instanceof cpp.Record) {
+//   } else if (cpp_type instanceof cpp.Field) {
+//   } else if (cpp_type instanceof cpp.Method) {
+//   } else if (cpp_type instanceof cpp.Function) {
+//   } else if (cpp_type instanceof cpp.Parameter) {
+//   }
+// });
 
 // run codegen
+
+class TestMetaLang {
+  // value assign
+  @ml.value('number')
+  number_val: number = 0;
+  @ml.value('boolean')
+  boolean_val: boolean = false;
+  @ml.value('string')
+  string_val: string = "";
+
+  // array assign
+  @ml.array('number')
+  number_arr_val: number[] = [];
+  @ml.array('boolean')
+  boolean_arr_val: boolean[] = [];
+  @ml.array('string')
+  string_arr_val: string[] = [];
+
+  // preset
+  @ml.preset('default')
+  preset_default() {
+    this.number_val = 0;
+    this.boolean_val = false;
+    this.string_val = "";
+
+    this.number_arr_val = [];
+    this.boolean_arr_val = [];
+    this.string_arr_val = [];
+  }
+
+  // sub object
+  sub: TestMetaLangSub = new TestMetaLangSub();
+}
+class TestMetaLangSub {
+  @ml.value('number')
+  value: number = 0;
+
+  @ml.array('number')
+  array_value: number[] = [];
+
+  @ml.value_proxy('number')
+  proxy_value(v: number) {
+    this.value = v;
+  }
+
+  @ml.array_proxy('number')
+  proxy_array(v: number[], is_append: boolean) {
+    if (is_append) {
+      this.array_value.push(...v);
+    } else {
+      this.array_value = v;
+    }
+  }
+};
+const compiler = ml.Compiler.load();
+const program = compiler.compile(
+  `
+  test.number_val = 114;
+  test.boolean_val = true;
+  test.string_val = "114514";
+
+  test = @default;
+
+  test.number_arr_val = [1, 2, 3];
+  test.boolean_arr_val = [true, false, true];
+  test.string_arr_val = ["1", "2", "3"];
+
+  test.sub = 114514;
+  test.sub = [1, 1, 4, 5, 1, 4];
+  `
+);
+const test_obj = {
+  test: new TestMetaLang()
+};
+program.exec(test_obj);
+console.log(test_obj);
--- tools/meta_codegen_ts/framework/meta_lang.ts
+++ tools/meta_codegen_ts/framework/meta_lang.ts
@@ -3,217 +3,13 @@ import * as peggy from "peggy";
 import * as fs from "node:fs";
 import * as path from "node:path";
 
-// meta lang parse result
-type Value = {
-  type: "value";
-  value: string | number | boolean;
-};
-type Array = {
-  type: "array";
-  value: (string | number | boolean)[];
-};
-type Preset = {
-  type: "preset";
-  value: string[];
-};
-type Expr = {
-  visitor: string[];
-  op: "=" | "+=";
-  value: Value | Array | Preset;
-};
-
-export class Parser {
-  #parser: peggy.Parser;
-
-  constructor(grammar: string) {
-    this.#parser = peggy.generate(grammar);
-  }
-
-  parse(input: string): Program {
-    try {
-      const exprs = this.#parser.parse(input);
-      return new Program(exprs);
-    } catch (e: any) {
-      throw new Error(
-        `failed to parse meta lang expression
-${e}
-source: ${input}
-at line ${e.location.start.line} column ${e.location.start.column}`,
-      );
-    }
-  }
-}
-
-export class Program {
-  exprs: Expr[];
-
-  constructor(exprs: Expr[]) {
-    this.exprs = exprs;
-  }
-
-  exec(obj: any, ctx: ExecContext) {
-    for (const expr of this.exprs) {
-      if (expr.op === "=") {
-        if (expr.value.type === "value" || expr.value.type === "array") {
-          Program.assign_value(obj, expr.visitor, expr.value);
-        } else if (expr.value.type === "preset") {
-          const visitor_key = expr.visitor.join(".");
-          for (const preset of expr.value.value) {
-            ctx.presets[visitor_key](preset, obj, PresetTools);
-          }
-        } else {
-          throw new Error(
-            `not support value ${expr.value} in meta lang`,
-          );
-        }
-      } else if (expr.op === "+=") {
-        if (expr.value.type === "value" || expr.value.type === "array") {
-          Program.append_value(obj, expr.visitor, expr.value);
-        } else if (expr.value.type === "preset") {
-          throw new Error(
-            `preset not support "+=" operator, visitor: ${expr.visitor.join(
-              ".",
-            )
-            }`,
-          );
-        } else {
-          throw new Error(
-            `not support value ${expr.value} in meta lang`,
-          );
-        }
-      }
-    }
-  }
-
-  static assign_value(obj: any, keys: string[], value: Value | Array) {
-    let cur_obj = obj;
-    keys.forEach((k, i) => {
-      if (i == keys.length - 1) {
-        // assign value
-        cur_obj[k] = value.value;
-      } else {
-        // create object
-        if (cur_obj[k] == undefined) {
-          cur_obj[k] = {};
-        }
-
-        // check type
-        if (typeof cur_obj[k] != "object") {
-          throw new Error(
-            `type conflict, old value is ${cur_obj[k]
-            }, excepted object, when assign "${keys.slice(0, i + 1).join(".")
-            }"`,
-          );
-        }
-
-        // update cur_obj
-        cur_obj = cur_obj[k];
-      }
-    });
-  }
-  static append_value(obj: any, keys: string[], value: Value | Array) {
-    let cur_obj = obj;
-    keys.forEach((k, i) => {
-      if (i == keys.length - 1) {
-        // create array
-        if (cur_obj[k] == undefined) {
-          cur_obj[k] = [];
-        }
-
-        // check type
-        if (!Array.isArray(cur_obj[k])) {
-          throw new Error(
-            `type conflict, old value is ${cur_obj[k]
-            }, excepted array, when assign "${keys.join(".")}"`,
-          );
-        }
-
-        // append value
-        if (value.type == "value") {
-          cur_obj[k].push(value.value);
-        } else if (value.type == "array") {
-          cur_obj[k].push(...value.value);
-        } else {
-          throw new Error(
-            `not support value ${value} for array append`,
-          );
-        }
-      } else {
-        // create object
-        if (cur_obj[k] == undefined) {
-          cur_obj[k] = {};
-        }
-
-        // check type
-        if (typeof cur_obj[k] != "object") {
-          throw new Error(
-            `type conflict, old value is ${cur_obj[k]
-            }, excepted object, when assign "${keys.slice(0, i + 1).join(".")
-            }"`,
-          );
-        }
-
-        // update cur_obj
-        cur_obj = cur_obj[k];
-      }
-    });
-  }
-}
-
-export function load_parser(): Parser {
-  return new Parser(
-    fs.readFileSync(
-      path.join(__dirname, "meta_lang.peggy"),
-      "utf-8",
-    ),
-  );
-}
-
-type PresetToolType = {
-  assign(obj: any, key: string, value: any): void;
-  append(obj: any, key: string, value: any): void;
-};
-export type PresetCallback = (
-  name: string,
-  obj: any,
-  tool: PresetToolType,
-) => void;
-class PresetTools {
-  static assign(obj: any, key: string, value: any) {
-    const keys = key.split(".");
-    const value_obj: Value = {
-      type: "value",
-      value: value,
-    };
-    Program.assign_value(obj, keys, value_obj);
-  }
-  static append(obj: any, key: string, value: any) {
-    const keys = key.split(".");
-    let value_obj: Value | Array;
-    if (Array.isArray(value)) {
-      value_obj = {
-        type: "array",
-        value: value,
-      };
-    } else {
-      value_obj = {
-        type: "value",
-        value: value,
-      };
-    }
-    Program.append_value(obj, keys, value_obj);
-  }
-}
-export class ExecContext {
-  presets: { [key: string]: PresetCallback } = {};
-}
-
 //======================== scheme ========================
 type ValueType = string | number | boolean;
 type ValueTypeStr = 'string' | 'number' | 'boolean';
 type PresetFunc = () => void;
 type ValueAssignFunc = (value: any) => void;
-type ArrayAssignFunc = (value: any[]) => void;
+type ArrayAssignFunc = (value: any[], is_append: boolean) => void;
+type CheckTypeType = { accept_type: ValueTypeStr; }
 type PresetData = {
   name: string;
   func: PresetFunc;
@@ -248,7 +44,7 @@ function check_and_assign(target: any, meta_symbol: symbol, name: string, value:
   const values = metadata[meta_symbol] as Dict<PresetData>;
 
   // solve name
-  if (typeof name != "string") throw new Error("preset name must be string");
+  if (typeof name != "string") throw new Error("name must be string");
 
   // check name conflict
   if (values[name]) throw new Error(`${String(meta_symbol)}[${name}] already exists`);
@@ -256,7 +52,7 @@ function check_and_assign(target: any, meta_symbol: symbol, name: string, value:
   // assign data
   values[name] = value;
 }
-export function ml_preset(name?: string) {
+export function preset(name?: string) {
   return (target: any, method_name: string, desc: TypedPropertyDescriptor<PresetFunc>) => {
     check_and_assign(
       target,
@@ -269,7 +65,7 @@ export function ml_preset(name?: string) {
     )
   }
 }
-export function ml_value(accept_type: ValueTypeStr, options: { name?: string } = {}) {
+export function value(accept_type: ValueTypeStr, options: { name?: string } = {}) {
   return (target: any, field_or_method: string, desc?: TypedPropertyDescriptor<ValueAssignFunc>) => {
     if (desc !== undefined) {
       check_and_assign(
@@ -296,7 +92,7 @@ export function ml_value(accept_type: ValueTypeStr, options: { name?: string } =
     }
   }
 }
-export function ml_array(accept_type: ValueTypeStr, options: { name?: string } = {}) {
+export function array(accept_type: ValueTypeStr, options: { name?: string } = {}) {
   return (target: any, field_or_method: string, desc?: TypedPropertyDescriptor<ArrayAssignFunc>) => {
     if (desc !== undefined) {
       check_and_assign(
@@ -323,29 +119,305 @@ export function ml_array(accept_type: ValueTypeStr, options: { name?: string } =
     }
   }
 }
-export function ml_value_proxy(accept_type: ValueTypeStr) {
+export function value_proxy(accept_type: ValueTypeStr) {
   return (target: any, method_name: string, desc: TypedPropertyDescriptor<ValueAssignFunc>) => {
-    check_and_assign(
-      target,
-      symbol_value_proxy,
-      method_name,
-      {
-        accept_type: accept_type,
-        func: desc.value,
-      } as ValueProxyData
-    )
+    const metadata = target[Symbol.metadata] ??= {};
+    metadata[symbol_value_proxy] = {
+      func: desc.value,
+      accept_type: accept_type,
+    } as ValueProxyData;
   }
 }
-export function ml_array_proxy(accept_type: ValueTypeStr) {
+export function array_proxy(accept_type: ValueTypeStr) {
   return (target: any, method_name: string, desc: TypedPropertyDescriptor<ArrayAssignFunc>) => {
-    check_and_assign(
-      target,
-      symbol_array_proxy,
-      method_name,
-      {
-        accept_type: accept_type,
-        func: desc.value,
-      } as ArrayProxyData
-    )
+    const metadata = target[Symbol.metadata] ??= {};
+    metadata[symbol_array_proxy] = {
+      func: desc.value,
+      accept_type: accept_type,
+    } as ArrayProxyData;;
+  }
+}
+
+//======================== compiler ========================
+export class Compiler {
+  #parser: peggy.Parser;
+
+  constructor(grammar: string) {
+    this.#parser = peggy.generate(grammar);
+  }
+
+  compile(input: string): Program {
+    try {
+      const exprs = this.#parser.parse(input);
+      return new Program(exprs);
+    } catch (e: any) {
+      e.location as peggy.Location;
+      throw new Error(
+        `failed to parse meta lang expression
+${e}
+source: ${input}
+at line ${e.location.start.line} column ${e.location.start.column}`,
+      );
+    }
+  }
+
+  static load() {
+    return new Compiler(
+      fs.readFileSync(
+        path.join(__dirname, "meta_lang.peggy"),
+        "utf-8",
+      ),
+    );
+  }
+}
+
+//======================== ast ========================
+type LiteralNode = {
+  type: 'literal';
+  value: ValueType;
+  location: peggy.LocationRange;
+};
+type IdentifierNode = {
+  type: 'identifier';
+  value: string;
+  location: peggy.LocationRange;
+};
+type AccessExprNode = {
+  type: 'access';
+  value: IdentifierNode[];
+  location: peggy.LocationRange;
+}
+type PresetNode = {
+  type: 'preset';
+  value: string;
+  location: peggy.LocationRange;
+}
+type PresetExprNode = {
+  type: 'preset_expr';
+  value: PresetNode[];
+  location: peggy.LocationRange;
+}
+type ArrayExprNode = {
+  type: 'array_expr';
+  value: LiteralNode[];
+  location: peggy.LocationRange;
+}
+type OperatorNode = {
+  type: 'operator';
+  op: "=" | "+=";
+  left: AccessExprNode;
+  right: LiteralNode | PresetExprNode | ArrayExprNode;
+  location: peggy.LocationRange;
+}
+
+//======================== program ========================
+class AccessFailedError extends Error {
+  key: string;
+  obj: any;
+  location: peggy.LocationRange;
+
+  constructor(key: string, obj: any, location: peggy.LocationRange) {
+    super(`failed to access ${key} in ${obj.constructor.name}
+  at script: ${location.start.line}：${location.start.column}`)
+
+    this.key = key;
+    this.obj = obj;
+    this.location = location;
+  }
+}
+class TypeMismatchError extends Error {
+  expected: string;
+  actual: any;
+  location: peggy.LocationRange;
+
+  constructor(expected: string, actual: any, location: peggy.LocationRange) {
+    super(`type mismatch, expected ${expected}, but got ${actual} (${typeof actual})
+  at script: ${location.start.line}：${location.start.column}`);
+    this.expected = expected;
+    this.actual = actual;
+    this.location = location;
+  }
+}
+class PresetNotFoundError extends Error {
+  preset: string;
+  obj: any;
+  location: peggy.LocationRange;
+
+  constructor(preset: string, obj: any, location: peggy.LocationRange) {
+    super(`preset ${preset} not found in ${obj.constructor.name}
+  at script: ${location.start.line}：${location.start.column}`);
+    this.preset = preset;
+    this.obj = obj;
+    this.location = location;
+  }
+}
+export class Program {
+  #exprs: OperatorNode[];
+
+  constructor(exprs: OperatorNode[]) {
+    this.#exprs = exprs;
+  }
+
+  exec(obj: any) {
+    for (const expr of this.#exprs) {
+      this.#exec_expr(obj, expr);
+    }
+  }
+  #exec_expr(obj: any, expr: OperatorNode) {
+    // process access
+    let cur_obj = obj;
+    let cur_key!: string;
+    expr.left.value.forEach((ident, idx) => {
+      cur_key = ident.value;
+      if (idx === expr.left.value.length - 1) return;
+      let next_obj = cur_obj[ident.value];
+      if (next_obj === undefined) {
+        throw new AccessFailedError(
+          ident.value,
+          cur_obj,
+          expr.left.location,
+        )
+      }
+      cur_obj = next_obj;
+    })
+
+    // process operator
+    if (expr.right.type === 'literal' || expr.right.type === 'array_expr') {
+      // get symbol, only literal assign will be trait as value
+      const is_array = expr.right.type === 'array_expr' || expr.op === '+=';
+      const access_sb = is_array ? symbol_array : symbol_value;
+      const proxy_sb = is_array ? symbol_array_proxy : symbol_value_proxy;
+
+      // find accessor
+      const metadata = Object.getPrototypeOf(cur_obj)[Symbol.metadata];
+      const value_data = metadata?.[access_sb]?.[cur_key];
+      if (value_data !== undefined) {
+        // assign field
+        Program.#check_type(value_data, expr);
+        Program.#do_assign(cur_obj, value_data, expr);
+        return;
+      } else {
+        // assign by proxy
+        const proxy_obj = cur_obj[cur_key];
+        if (proxy_obj !== undefined) {
+          const proxy_metadata = Object.getPrototypeOf(proxy_obj)[Symbol.metadata];
+          const proxy_value_data = proxy_metadata?.[proxy_sb];
+          if (proxy_value_data !== undefined) {
+            proxy_value_data as ValueProxyData;
+            Program.#check_type(proxy_value_data, expr);
+            Program.#do_proxy(proxy_obj, proxy_value_data, expr);
+            return;
+          }
+        }
+      }
+
+    } else if (expr.right.type === "preset_expr") {
+      // find preset expr
+      const preset_obj = cur_obj[cur_key];
+      if (preset_obj !== undefined) {
+        const metadata = Object.getPrototypeOf(preset_obj)[Symbol.metadata];
+        const preset_data_map = metadata?.[symbol_preset];
+        if (preset_data_map !== undefined) {
+          // each preset and assign
+          for (const preset_node of expr.right.value) {
+            const preset_data = preset_data_map[preset_node.value];
+            if (preset_data === undefined) {
+              throw new PresetNotFoundError(
+                preset_node.value,
+                preset_obj,
+                preset_node.location,
+              );
+            } else {
+              preset_data.func.call(preset_obj);
+            }
+          }
+          return;
+        }
+      }
+
+    } else {
+      throw new Error("should not reach here");
+    }
+
+    // failed in all access case
+    throw new AccessFailedError(
+      cur_key,
+      cur_obj,
+      expr.left.location,
+    );
+  }
+  static #check_type(data: CheckTypeType, expr: OperatorNode) {
+    if (expr.right.type === 'literal') {
+      // if literal, check value
+      if (data.accept_type !== typeof expr.right.value) {
+        throw new TypeMismatchError(
+          data.accept_type,
+          expr.right.value,
+          expr.right.location,
+        );
+      }
+    } else if (expr.right.type === 'array_expr') {
+      // if array, check items
+      for (const item of expr.right.value) {
+        if (data.accept_type !== typeof item.value) {
+          throw new TypeMismatchError(
+            data.accept_type,
+            item.value,
+            item.location,
+          );
+        }
+      }
+    } else {
+      throw new Error('should not reach here');
+    }
+  }
+  static #do_assign(obj: any, data: ValueData | ArrayData, expr: OperatorNode) {
+    const is_append = expr.op === '+='
+    if (expr.right.type === 'literal') {
+      if (is_append) { // literal append
+        if (typeof data.field_or_method === 'string') {
+          obj[data.field_or_method].push(expr.right.value);
+        } else {
+          const func = data.field_or_method as ArrayAssignFunc;
+          func.call(obj, [expr.right.value], true);
+        }
+      } else {  // literal assign
+        if (typeof data.field_or_method === 'string') {
+          obj[data.field_or_method] = expr.right.value;
+        } else {
+          const func = data.field_or_method as ValueAssignFunc;
+          func.call(obj, expr.right.value);
+        }
+      }
+    } else if (expr.right.type === 'array_expr') {
+      const array_param = expr.right.value.map((item) => item.value)
+
+      if (typeof data.field_or_method === 'string') {
+        if (is_append) { // array append
+          obj[data.field_or_method].push(...array_param);
+        } else { // array assign
+          obj[data.field_or_method] = array_param;
+        }
+      } else {
+        const func = data.field_or_method as ArrayAssignFunc;
+        func.call(obj, array_param, is_append);
+      }
+    } else {
+      throw new Error('should not reach here');
+    }
+  }
+  static #do_proxy(obj: any, data: ValueProxyData | ArrayProxyData, expr: OperatorNode) {
+    const is_append = expr.op === '+='
+
+
+    if (expr.right.type === 'literal') {
+      const typed_data = data as ValueProxyData;
+      typed_data.func.call(obj, expr.right.value);
+    } else if (expr.right.type === 'array_expr') {
+      const typed_data = data as ArrayProxyData;
+      typed_data.func.call(obj, expr.right.value.map((item) => item.value), false);
+    } else {
+      throw new Error('should not reach here');
+    }
   }
 }
