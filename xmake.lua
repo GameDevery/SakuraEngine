b8079d20b4f6eb995f0e53b269f4afca455f2963
Yuefeng Geng <maxwellgeng@outlook.com>
2025-03-10 15:35:11 +0800

    Update mimalloc to v3

--- xmake.lua
+++ xmake.lua
@@ -57,7 +57,7 @@ skr_global_target()
         end
     })
     skr_install("download", {
-        name = "meta-v1.0.1-llvm_18.1.6",
+        name = "meta-v1.0.1-llvm_19.1.7",
         install_func = "tool",
     })
     skr_install("download", {
