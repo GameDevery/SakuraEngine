78158fa8eb2760ea9600e66126997d5550537fb4
rong <211103376@qq.com>
2025-03-13 18:00:03 +0800

    match suggestion

--- modules/engine/v8/include/SkrV8/v8_matcher.hpp
+++ modules/engine/v8/include/SkrV8/v8_matcher.hpp
@@ -6,145 +6,286 @@
 
 namespace skr
 {
-enum class EConvertKind
-{
-    None = 0,
-    Primitive,
-    Box,
-    Wrap,
-};
-struct MatchSuggestion;
-struct MatchSuggestionPrimitive {
-    uint32_t    size      = 0;
-    uint32_t    alignment = 0;
-    ::skr::GUID type_id   = {};
-};
-template <typename T>
-struct MatchSuggestionWithField {
-    const ::skr::rttr::FieldData* field;
-    const ::skr::rttr::Type*      field_owner;
-    T                             suggestion;
-};
-struct MatchSuggestionBox {
-    using PrimitiveMember = MatchSuggestionWithField<MatchSuggestion>;
-    using BoxMember       = MatchSuggestionWithField<MatchSuggestion>;
+struct V8MatchSuggestion {
+    enum class EKind
+    {
+        None = 0,
+        Primitive,
+        Box,
+        Wrap,
+    };
+    struct Primitive {
+        uint32_t    size      = 0;
+        uint32_t    alignment = 0;
+        ::skr::GUID type_id   = {};
+    };
+    template <typename T>
+    struct WithField {
+        const ::skr::rttr::FieldData* field;
+        const ::skr::rttr::Type*      field_owner;
+        T                             suggestion;
+    };
+    struct Box {
+        using PrimitiveMember = WithField<V8MatchSuggestion>;
+        using BoxMember       = WithField<V8MatchSuggestion>;
 
-    ::skr::rttr::Type*      type = nullptr;
-    Vector<PrimitiveMember> primitive_members;
-    Vector<BoxMember>       box_members;
-};
-struct MatchSuggestionWrap {
-    ::skr::rttr::Type* type = nullptr;
-    // TODO. matches data
-};
-struct MatchSuggestion {
+        ::skr::rttr::Type*      type = nullptr;
+        Vector<PrimitiveMember> primitive_members;
+        Vector<BoxMember>       box_members;
+    };
+    struct Wrap {
+        ::skr::rttr::Type* type = nullptr;
+    };
+
     // factory
     template <typename T>
-    inline static MatchSuggestion Primitive()
+    inline static V8MatchSuggestion FromPrimitive()
     {
-        MatchSuggestion result;
+        V8MatchSuggestion result;
 
         // setup kind
-        result.kind = EConvertKind::Primitive;
-        new (&result.primitive) MatchSuggestionPrimitive();
+        result._kind = EKind::Primitive;
+        new (&result._primitive) Primitive();
 
         // setup primitive data
-        result.primitive.size      = sizeof(T);
-        result.primitive.alignment = alignof(T);
-        result.primitive.type_id   = ::skr::rttr::type_id_of<T>();
+        result._primitive.size      = sizeof(T);
+        result._primitive.alignment = alignof(T);
+        result._primitive.type_id   = ::skr::rttr::type_id_of<T>();
+
+        return result;
+    }
+    inline static V8MatchSuggestion FromBox(::skr::rttr::Type* type)
+    {
+        V8MatchSuggestion result;
+
+        // setup kind
+        result._kind = EKind::Box;
+        new (&result._box) Box();
+
+        // setup box data
+        result._box.type = type;
+
+        return result;
+    }
+    inline static V8MatchSuggestion FromWrap(::skr::rttr::Type* type)
+    {
+        V8MatchSuggestion result;
+
+        // setup kind
+        result._kind = EKind::Wrap;
+        new (&result._wrap) Wrap();
+
+        // setup wrap data
+        result._wrap.type = type;
 
         return result;
     }
 
     // ctor & dtor
-    inline MatchSuggestion() {}
-    inline ~MatchSuggestion()
+    inline V8MatchSuggestion() {}
+    inline ~V8MatchSuggestion() { reset(); }
+
+    // move & copy
+    inline V8MatchSuggestion(const V8MatchSuggestion& other)
     {
-        switch (kind)
+        _kind = other._kind;
+        switch (_kind)
         {
-        case EConvertKind::Primitive:
-            primitive.~MatchSuggestionPrimitive();
+        case EKind::Primitive:
+            new (&_primitive) Primitive(other._primitive);
             break;
-        case EConvertKind::Box:
-            box.~MatchSuggestionBox();
+        case EKind::Box:
+            new (&_box) Box(other._box);
             break;
-        case EConvertKind::Wrap:
-            wrap.~MatchSuggestionWrap();
+        case EKind::Wrap:
+            new (&_wrap) Wrap(other._wrap);
+            break;
+        default:
+            break;
+        }
+    }
+    inline V8MatchSuggestion(V8MatchSuggestion&& other)
+    {
+        _kind = other._kind;
+        switch (_kind)
+        {
+        case EKind::Primitive:
+            new (&_primitive) Primitive(std::move(other._primitive));
+            break;
+        case EKind::Box:
+            new (&_box) Box(std::move(other._box));
+            break;
+        case EKind::Wrap:
+            new (&_wrap) Wrap(std::move(other._wrap));
             break;
         default:
             break;
         }
     }
 
-    // move & copy
-    inline MatchSuggestion(const MatchSuggestion& other)
+    // assign & move assign
+    inline V8MatchSuggestion& operator=(const V8MatchSuggestion& other)
     {
-        kind = other.kind;
-        switch (kind)
+        if (this != &other)
         {
-        case EConvertKind::Primitive:
-            new (&primitive) MatchSuggestionPrimitive(other.primitive);
-            break;
-        case EConvertKind::Box:
-            new (&box) MatchSuggestionBox(other.box);
-            break;
-        case EConvertKind::Wrap:
-            new (&wrap) MatchSuggestionWrap(other.wrap);
-            break;
-        default:
-            break;
+            reset();
+            _kind = other._kind;
+            switch (_kind)
+            {
+            case EKind::Primitive:
+                new (&_primitive) Primitive(other._primitive);
+                break;
+            case EKind::Box:
+                new (&_box) Box(other._box);
+                break;
+            case EKind::Wrap:
+                new (&_wrap) Wrap(other._wrap);
+                break;
+            default:
+                break;
+            }
         }
+        return *this;
     }
-    inline MatchSuggestion(MatchSuggestion&& other)
+    inline V8MatchSuggestion& operator=(V8MatchSuggestion&& other)
     {
-        kind = other.kind;
-        switch (kind)
+        if (this != &other)
         {
-        case EConvertKind::Primitive:
-            new (&primitive) MatchSuggestionPrimitive(std::move(other.primitive));
+            reset();
+            _kind = other._kind;
+            switch (_kind)
+            {
+            case EKind::Primitive:
+                new (&_primitive) Primitive(std::move(other._primitive));
+                break;
+            case EKind::Box:
+                new (&_box) Box(std::move(other._box));
+                break;
+            case EKind::Wrap:
+                new (&_wrap) Wrap(std::move(other._wrap));
+                break;
+            default:
+                break;
+            }
+        }
+        return *this;
+    }
+
+    // operators
+    inline void reset()
+    {
+        switch (_kind)
+        {
+        case EKind::Primitive:
+            _primitive.~Primitive();
             break;
-        case EConvertKind::Box:
-            new (&box) MatchSuggestionBox(std::move(other.box));
+        case EKind::Box:
+            _box.~Box();
             break;
-        case EConvertKind::Wrap:
-            new (&wrap) MatchSuggestionWrap(std::move(other.wrap));
+        case EKind::Wrap:
+            _wrap.~Wrap();
             break;
         default:
             break;
         }
+        _kind = EKind::None;
     }
 
     // validator
-    inline bool is_empty() const { return kind == EConvertKind::None; }
-    inline bool is_primitive() const { return kind == EConvertKind::Primitive; }
-    inline bool is_box() const { return kind == EConvertKind::Box; }
-    inline bool is_wrap() const { return kind == EConvertKind::Wrap; }
+    inline bool is_empty() const { return _kind == EKind::None; }
+    inline bool is_primitive() const { return _kind == EKind::Primitive; }
+    inline bool is_box() const { return _kind == EKind::Box; }
+    inline bool is_wrap() const { return _kind == EKind::Wrap; }
 
-public:
-    EConvertKind kind = EConvertKind::None;
+    // getter
+    inline EKind            kind() const { return _kind; }
+    inline const Primitive& primitive() const
+    {
+        SKR_ASSERT(_kind == EKind::Primitive);
+        return _primitive;
+    }
+    inline Primitive& primitive()
+    {
+        SKR_ASSERT(_kind == EKind::Primitive);
+        return _primitive;
+    }
+    inline const Box& box() const
+    {
+        SKR_ASSERT(_kind == EKind::Box);
+        return _box;
+    }
+    inline Box& box()
+    {
+        SKR_ASSERT(_kind == EKind::Box);
+        return _box;
+    }
+    inline const Wrap& wrap() const
+    {
+        SKR_ASSERT(_kind == EKind::Wrap);
+        return _wrap;
+    }
+    inline Wrap& wrap()
+    {
+        SKR_ASSERT(_kind == EKind::Wrap);
+        return _wrap;
+    }
+
+private:
+    EKind _kind = EKind::None;
     union
     {
-        MatchSuggestionPrimitive primitive;
-        MatchSuggestionBox       box;
-        MatchSuggestionWrap      wrap;
+        Primitive _primitive;
+        Box       _box;
+        Wrap      _wrap;
     };
 };
 
-struct V8Matcher {
+struct SKR_V8_API V8Matcher {
+    // match
     //! NOTE. matcher will ignore decayed pointer modifiers, you should check it outside
     //! NOTE. because the behaviour of decayed pointer is depend on where the type is used (param/field...etc)
-    MatchSuggestion match_to_native(::v8::Local<::v8::Value> v8_value, rttr::TypeSignatureView signature);
-    MatchSuggestion match_to_v8(rttr::TypeSignatureView signature);
+    V8MatchSuggestion match_to_native(::v8::Local<::v8::Value> v8_value, rttr::TypeSignatureView signature);
+    V8MatchSuggestion match_to_v8(rttr::TypeSignatureView signature);
 
     // convert
     static ::v8::Local<::v8::Value> conv_to_v8(
-        const MatchSuggestion& suggestion,
-        void*                  native_data
+        const V8MatchSuggestion& suggestion,
+        void*                    native_data
     );
     static bool conv_to_native(
-        const MatchSuggestion&   suggestion,
+        const V8MatchSuggestion& suggestion,
         void*                    native_data,
         ::v8::Local<::v8::Value> v8_value,
+        bool                     is_init
+    );
+
+private:
+    // match helper
+    static V8MatchSuggestion _suggest_primitive(GUID type_id);
+    V8MatchSuggestion _suggest_box(rttr::Type* type);
+    V8MatchSuggestion _suggest_wrap(rttr::Type* type);
+    bool _match_primitive(const V8MatchSuggestion& suggestion, v8::Local<v8::Value> v8_value);
+    bool _match_box(const V8MatchSuggestion& suggestion, v8::Local<v8::Value> v8_value);
+
+    // convert helpers
+    static ::v8::Local<::v8::Value> _to_v8_primitive(
+        const V8MatchSuggestion& suggestion,
+        void*                    native_data
+    );
+    static bool _to_native_primitive(
+        const V8MatchSuggestion& suggestion,
+        ::v8::Local<::v8::Value> v8_value,
+        void*                    native_data,
+        bool                     is_init
+    );
+    static ::v8::Local<::v8::Value> _to_v8_box(
+        const V8MatchSuggestion& suggestion,
+        void*                    native_data
+    );
+    static bool _to_native_box(
+        const V8MatchSuggestion& suggestion,
+        ::v8::Local<::v8::Value> v8_value,
+        void*                    native_data,
         bool                     is_init
     );
 };
--- modules/engine/v8/src/v8_matcher.cpp
+++ modules/engine/v8/src/v8_matcher.cpp
@@ -1,15 +1,274 @@
 #include "SkrV8/v8_matcher.hpp"
+#include "SkrCore/log.hpp"
 #include "SkrV8/v8_bind_tools.hpp"
+#include "v8-external.h"
 
-// helpers
 namespace skr
 {
-inline static ::v8::Local<::v8::Value> _to_v8_primitive(
-    const MatchSuggestion& suggestion,
-    void*                  native_data
+// match
+V8MatchSuggestion V8Matcher::match_to_native(::v8::Local<::v8::Value> v8_value, rttr::TypeSignatureView signature)
+{
+    auto isolate = ::v8::Isolate::GetCurrent();
+    auto context = isolate->GetCurrentContext();
+
+    if (signature.is_type())
+    {
+        // read info
+        signature.jump_modifier();
+        GUID type_id;
+        signature.read_type_id(type_id);
+
+        // match primitive
+        V8MatchSuggestion result = _suggest_primitive(type_id);
+        if (!result.is_empty())
+        {
+            if (_match_primitive(result, v8_value))
+            {
+                return result;
+            }
+        }
+
+        // match box
+        rttr::Type* type = rttr::get_type_from_guid(type_id);
+        result           = _suggest_box(type);
+        if (!result.is_empty())
+        {
+            if (_match_box(result, v8_value))
+            {
+                return result;
+            }
+        }
+
+        // match wrap
+        result = _suggest_wrap(type);
+        if (!result.is_empty())
+        {
+            // check v8 value type
+            if (v8_value->IsObject())
+            {
+                // check wrap data
+                auto v8_obj = v8_value->ToObject(context).ToLocalChecked();
+                if (v8_obj->InternalFieldCount() >= 1)
+                {
+                    void*             raw_bind_core = v8_obj->GetInternalField(0).As<v8::External>()->Value();
+                    V8BindRecordCore* bind_core     = reinterpret_cast<V8BindRecordCore*>(raw_bind_core);
+                    
+                    // check inherit
+                    if (bind_core->type->based_on(type->type_id()))
+                    {
+                        return result;
+                    }
+                }
+            }
+        }
+    }
+    else if (signature.is_generic_type())
+    {
+        // TODO. support generic
+    }
+
+    return {};
+}
+V8MatchSuggestion V8Matcher::match_to_v8(rttr::TypeSignatureView signature)
+{
+    if (signature.is_type())
+    {
+        // read info
+        signature.jump_modifier();
+        GUID type_id;
+        signature.read_type_id(type_id);
+
+        // match primitive
+        V8MatchSuggestion result = _suggest_primitive(type_id);
+        if (!result.is_empty()) return result;
+
+        // match box
+        rttr::Type* type = rttr::get_type_from_guid(type_id);
+        result           = _suggest_box(type);
+        if (!result.is_empty()) { return result; }
+
+        // match wrap
+        result = _suggest_wrap(type);
+        if (!result.is_empty()) { return result; }
+    }
+    else if (signature.is_generic_type())
+    {
+        // TODO. support generic
+    }
+
+    return {};
+}
+
+// convert
+::v8::Local<::v8::Value> V8Matcher::conv_to_v8(
+    const V8MatchSuggestion& suggestion,
+    void*                    native_data
 )
 {
-    switch (suggestion.primitive.type_id.get_hash())
+    return {};
+}
+bool V8Matcher::conv_to_native(
+    const V8MatchSuggestion& suggestion,
+    void*                    native_data,
+    ::v8::Local<::v8::Value> v8_value,
+    bool                     is_init
+)
+{
+    return false;
+}
+
+// match helper
+V8MatchSuggestion V8Matcher::_suggest_primitive(GUID type_id)
+{
+    switch (type_id.get_hash())
+    {
+    case rttr::type_id_of<int8_t>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<int8_t>();
+    case rttr::type_id_of<int16_t>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<int16_t>();
+    case rttr::type_id_of<int32_t>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<int32_t>();
+    case rttr::type_id_of<int64_t>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<int64_t>();
+    case rttr::type_id_of<uint8_t>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<uint8_t>();
+    case rttr::type_id_of<uint16_t>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<uint16_t>();
+    case rttr::type_id_of<uint32_t>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<uint32_t>();
+    case rttr::type_id_of<uint64_t>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<uint64_t>();
+    case rttr::type_id_of<float>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<float>();
+    case rttr::type_id_of<double>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<double>();
+    case rttr::type_id_of<bool>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<bool>();
+    case rttr::type_id_of<skr::String>().get_hash():
+        return V8MatchSuggestion::FromPrimitive<skr::String>();
+    default:
+        return {};
+    }
+}
+V8MatchSuggestion V8Matcher::_suggest_box(rttr::Type* type)
+{
+    if (!flag_all(type->record_flag(), rttr::ERecordFlag::ScriptBox)) { return {}; }
+
+    V8MatchSuggestion result     = V8MatchSuggestion::FromBox(type);
+    auto&             box_result = result.box();
+
+    // each field
+    type->each_field([&](const rttr::FieldData* field, const rttr::Type* owner_type) {
+        // check visible
+        if (!flag_all(field->flag, rttr::EFieldFlag::ScriptVisible)) { return; }
+
+        // get type signature
+        auto signature = field->type.view();
+
+        // try export
+        if (signature.is_decayed_pointer())
+        {
+            // TODO. support wrap type
+        }
+        else
+        {
+            skr::GUID type_id;
+            signature.jump_modifier();
+            signature.read_type_id(type_id);
+
+            // try primitive
+            auto suggestion = _suggest_primitive(type_id);
+            if (!suggestion.is_empty())
+            {
+                box_result.primitive_members.push_back({
+                    .field       = field,
+                    .field_owner = owner_type,
+                    .suggestion  = suggestion,
+                });
+                return;
+            }
+
+            // try box
+            auto* field_type  = rttr::get_type_from_guid(type_id);
+            auto  suggestions = _suggest_box(field_type);
+            if (!suggestions.is_empty())
+            {
+                box_result.box_members.push_back({
+                    .field       = field,
+                    .field_owner = owner_type,
+                    .suggestion  = suggestions,
+                });
+                return;
+            }
+        }
+
+        // failed to export
+        SKR_LOG_FMT_ERROR(
+            u8"failed to export field {}::{}",
+            owner_type->name(),
+            field->name
+        );
+    });
+
+    return result;
+}
+V8MatchSuggestion V8Matcher::_suggest_wrap(rttr::Type* type)
+{
+    if (flag_all(type->record_flag(), rttr::ERecordFlag::ScriptVisible))
+    {
+        if (type->based_on(rttr::type_id_of<rttr::ScriptbleObject>()))
+        {
+            return V8MatchSuggestion::FromWrap(type);
+        }
+    }
+
+    return {};
+}
+bool V8Matcher::_match_primitive(const V8MatchSuggestion& suggestion, v8::Local<v8::Value> v8_value)
+{
+    switch (suggestion.primitive().type_id.get_hash())
+    {
+    case rttr::type_id_of<int8_t>().get_hash():
+        return v8_value->IsInt32();
+    case rttr::type_id_of<int16_t>().get_hash():
+        return v8_value->IsInt32();
+    case rttr::type_id_of<int32_t>().get_hash():
+        return v8_value->IsInt32();
+    case rttr::type_id_of<int64_t>().get_hash():
+        return v8_value->IsBigInt();
+    case rttr::type_id_of<uint8_t>().get_hash():
+        return v8_value->IsUint32();
+    case rttr::type_id_of<uint16_t>().get_hash():
+        return v8_value->IsUint32();
+    case rttr::type_id_of<uint32_t>().get_hash():
+        return v8_value->IsUint32();
+    case rttr::type_id_of<uint64_t>().get_hash():
+        return v8_value->IsBigInt();
+    case rttr::type_id_of<float>().get_hash():
+        return v8_value->IsNumber();
+    case rttr::type_id_of<double>().get_hash():
+        return v8_value->IsNumber();
+    case rttr::type_id_of<bool>().get_hash():
+        return v8_value->IsBoolean();
+    case rttr::type_id_of<skr::String>().get_hash():
+        return v8_value->IsString();
+    default:
+        SKR_UNREACHABLE_CODE()
+        return false;
+    }
+}
+bool V8Matcher::_match_box(const V8MatchSuggestion& suggestion, v8::Local<v8::Value> v8_value)
+{
+    return false;
+}
+
+// convert helpers
+v8::Local<v8::Value> V8Matcher::_to_v8_primitive(
+    const V8MatchSuggestion& suggestion,
+    void*                    native_data
+)
+{
+    switch (suggestion.primitive().type_id.get_hash())
     {
     case rttr::type_id_of<int8_t>().get_hash():
         return V8BindTools::to_v8(*reinterpret_cast<int8_t*>(native_data));
@@ -40,14 +299,14 @@ inline static ::v8::Local<::v8::Value> _to_v8_primitive(
         return {};
     }
 }
-inline static bool _to_native_primitive(
-    const MatchSuggestion&   suggestion,
-    ::v8::Local<::v8::Value> v8_value,
+bool V8Matcher::_to_native_primitive(
+    const V8MatchSuggestion& suggestion,
+    v8::Local<v8::Value>     v8_value,
     void*                    native_data,
     bool                     is_init
 )
 {
-    switch (suggestion.primitive.type_id.get_hash())
+    switch (suggestion.primitive().type_id.get_hash())
     {
     case rttr::type_id_of<int8_t>().get_hash():
         return V8BindTools::to_native(v8_value, *reinterpret_cast<int8_t*>(native_data));
@@ -82,20 +341,21 @@ inline static bool _to_native_primitive(
         return false;
     }
 }
-inline static ::v8::Local<::v8::Value> _to_v8_box(
-    const MatchSuggestion& suggestion,
-    void*                  native_data
+v8::Local<v8::Value> V8Matcher::_to_v8_box(
+    const V8MatchSuggestion& suggestion,
+    void*                    native_data
 )
 {
-    auto isolate = ::v8::Isolate::GetCurrent();
+    auto isolate = v8::Isolate::GetCurrent();
     auto context = isolate->GetCurrentContext();
 
-    auto* type = suggestion.box.type;
+    auto& box_data = suggestion.box();
+    auto* type     = box_data.type;
 
-    auto result = ::v8::Object::New(isolate);
+    auto result = v8::Object::New(isolate);
 
     // primitive
-    for (const auto& data : suggestion.box.primitive_members)
+    for (const auto& data : box_data.primitive_members)
     {
         // get field info
         auto  field               = data.field;
@@ -108,13 +368,13 @@ inline static ::v8::Local<::v8::Value> _to_v8_box(
         result->Set(
             context,
             V8BindTools::to_v8(field->name, true),
-            V8Matcher::conv_to_v8(data.suggestion, field_address)
+            conv_to_v8(data.suggestion, field_address)
         ).Check();
         // clang-format on
     }
 
     // box
-    for (const auto& data : suggestion.box.box_members)
+    for (const auto& data : box_data.box_members)
     {
         // get field info
         auto  field               = data.field;
@@ -127,24 +387,25 @@ inline static ::v8::Local<::v8::Value> _to_v8_box(
         result->Set(
             context,
             V8BindTools::to_v8(field->name, true),
-            V8Matcher::conv_to_v8(data.suggestion, field_address)
+            conv_to_v8(data.suggestion, field_address)
         ).Check();
         // clang-format on
     }
 
     return result;
 }
-inline static bool _to_native_box(
-    const MatchSuggestion&   suggestion,
-    ::v8::Local<::v8::Value> v8_value,
+bool V8Matcher::_to_native_box(
+    const V8MatchSuggestion& suggestion,
+    v8::Local<v8::Value>     v8_value,
     void*                    native_data,
     bool                     is_init
 )
 {
-    auto isolate = ::v8::Isolate::GetCurrent();
+    auto isolate = v8::Isolate::GetCurrent();
     auto context = isolate->GetCurrentContext();
 
-    auto* type      = suggestion.box.type;
+    auto& box_data  = suggestion.box();
+    auto* type      = box_data.type;
     auto  v8_object = v8_value->ToObject(context).ToLocalChecked();
 
     // do init
@@ -156,7 +417,7 @@ inline static bool _to_native_box(
     }
 
     // primitive
-    for (const auto& data : suggestion.box.primitive_members)
+    for (const auto& data : box_data.primitive_members)
     {
         // get field info
         auto  field               = data.field;
@@ -173,11 +434,11 @@ inline static bool _to_native_box(
         // clang-format on
 
         // to native
-        V8Matcher::conv_to_native(data.suggestion, field_address, v8_field, true);
+        conv_to_native(data.suggestion, field_address, v8_field, true);
     }
 
     // box
-    for (const auto& data : suggestion.box.box_members)
+    for (const auto& data : box_data.box_members)
     {
         // get field info
         auto  field               = data.field;
@@ -194,7 +455,7 @@ inline static bool _to_native_box(
         // clang-format on
 
         // to native
-        V8Matcher::conv_to_native(data.suggestion, field_address, v8_field, true);
+        conv_to_native(data.suggestion, field_address, v8_field, true);
     }
 
     return true;
