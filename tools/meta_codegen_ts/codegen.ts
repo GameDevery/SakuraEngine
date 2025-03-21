751d5f00297f5d40cdb8cc459ec0a222abb54618
rong <211103376@qq.com>
2025-03-21 23:17:49 +0800

    some meta codegen TODO

--- tools/meta_codegen_ts/codegen.ts
+++ tools/meta_codegen_ts/codegen.ts
@@ -32,4 +32,42 @@ generator.codegen()
 // output
 generator.output()
 
+// TODO. use ErrorTracker to track meta-lang error, instead of using throw
+// TODO. use low level api to print log
+// class LogHelper {
+//   static red(str: string) {
+//     return `\x1b[31m${str}\x1b[0m`;
+//   }
+//   static green(str: string) {
+//     return `\x1b[32m${str}\x1b[0m`;
+//   }
+//   static yellow(str: string) {
+//     return `\x1b[33m${str}\x1b[0m`;
+//   }
+//   static blue(str: string) {
+//     return `\x1b[34m${str}\x1b[0m`;
+//   }
+//   static magenta(str: string) {
+//     return `\x1b[35m${str}\x1b[0m`;
+//   }
+//   static cyan(str: string) {
+//     return `\x1b[36m${str}\x1b[0m`;
+//   }
+//   static white(str: string) {
+//     return `\x1b[37m${str}\x1b[0m`;
+//   }
+//   static gray(str: string) {
+//     return `\x1b[30m${str}\x1b[0m`;
+//   }
+// }
+// await Bun.write(Bun.stderr, LogHelper.red("red\n"))
+// await Bun.write(Bun.stderr, LogHelper.green("green\n"))
+// await Bun.write(Bun.stderr, LogHelper.yellow("yellow\n"))
+// await Bun.write(Bun.stderr, LogHelper.blue("blue\n"))
+// await Bun.write(Bun.stderr, LogHelper.magenta("magenta\n"))
+// await Bun.write(Bun.stderr, LogHelper.cyan("cyan\n"))
+// await Bun.write(Bun.stderr, LogHelper.white("white\n"))
+// await Bun.write(Bun.stderr, LogHelper.gray("gray\n"))
 
+// TODO. use process.exit control output manually
+// process.exit()
