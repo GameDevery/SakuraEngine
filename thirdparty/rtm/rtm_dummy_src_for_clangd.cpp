3fd5f42ac2fe168098943ab90a73870e77eb9304
rong <211103376@qq.com>
2025-04-24 21:14:24 +0800

    optimize rtm clangd

---
+++ thirdparty/rtm/rtm_dummy_src_for_clangd.cpp
@@ -0,0 +1,2 @@
+// for clangd
+static_assert(false, "This file is only for clangd to parse rtm code, should not be compiled.");
--- thirdparty/rtm/xmake.lua
+++ thirdparty/rtm/xmake.lua
@@ -3,4 +3,5 @@ target("rtm")
 
     -- add packages
     add_includedirs("include", {public = true})
+    add_files("rtm_dummy_src_for_clangd.cpp")
     skr_dbg_natvis_files("rtm.natvis")
