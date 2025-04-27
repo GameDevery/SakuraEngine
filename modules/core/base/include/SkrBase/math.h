febf3c65874a92d1e83ca794110de58a3eadd20a
rong <211103376@qq.com>
2025-04-27 22:47:33 +0800

    some vector math

--- modules/core/base/include/SkrBase/math.h
+++ modules/core/base/include/SkrBase/math.h
@@ -8,6 +8,8 @@
     #include "./math/manual/transform.hpp"
     #include "./math/manual/rotator.hpp"
     #include "./math/manual/matrix.hpp"
+    #include "./math/manual/vector.hpp"
+    #include "./math/manual/matrix_utils.hpp"
 #else
     #include "./math/gen//gen_math_c_decl.h"
 #endif
