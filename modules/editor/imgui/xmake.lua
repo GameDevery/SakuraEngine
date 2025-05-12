10496cb1e95bf664a6113bf7dde531b6c8b3ac07
rong <211103376@qq.com>
2025-05-12 22:17:32 +0800

    SkrImGUING module

---
+++ modules/editor/imgui/xmake.lua
@@ -0,0 +1,20 @@
+shared_module("SkrImGuiNG", "SKR_IMGUI_NG")
+    -- misc
+    skr_unity_build()
+
+    -- deps
+    add_deps("ImGui")
+    public_dependency("SkrInput")
+    public_dependency("SkrRenderGraph")
+
+    -- includes
+    add_includedirs("include", {public=true})
+    add_files("src/**.cpp")
+
+    -- font files
+    skr_install_rule()
+    skr_install("download", {
+        name = "SourceSansPro-Regular.ttf",
+        install_func = "file",
+        out_dir = "resources/font"
+    })
