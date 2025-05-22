0b7a68c0b4b1379746be9f58cf643b007246bf51
rong <211103376@qq.com>
2025-05-22 15:58:46 +0800

    remove input.h from core

--- samples/application/ogui2/sandbox/src/input_binding.cpp
+++ samples/application/ogui2/sandbox/src/input_binding.cpp
@@ -16,26 +16,26 @@ inline static void _fill_pointer_event(PointerEvent* event)
 inline static void _bind_mouse_button(skr::input::InputSystem* system, RC<input::InputMappingContext> ctx, Sandbox* sandbox, EPointerButton button)
 {
     // translate key
-    EMouseKey key;
+    input::EMouseKey key;
     switch (button)
     {
     case EPointerButton::Left:
-        key = EMouseKey::MOUSE_KEY_LB;
+        key = input::EMouseKey::MOUSE_KEY_LB;
         break;
     case EPointerButton::Right:
-        key = EMouseKey::MOUSE_KEY_RB;
+        key = input::EMouseKey::MOUSE_KEY_RB;
         break;
     case EPointerButton::Middle:
-        key = EMouseKey::MOUSE_KEY_MB;
+        key = input::EMouseKey::MOUSE_KEY_MB;
         break;
     case EPointerButton::X1B:
-        key = EMouseKey::MOUSE_KEY_X1B;
+        key = input::EMouseKey::MOUSE_KEY_X1B;
         break;
     case EPointerButton::X2B:
-        key = EMouseKey::MOUSE_KEY_X2B;
+        key = input::EMouseKey::MOUSE_KEY_X2B;
         break;
     default:
-        key = EMouseKey::MOUSE_KEY_None;
+        key = input::EMouseKey::MOUSE_KEY_None;
         break;
     }
 
@@ -86,7 +86,7 @@ void bind_pointer_event(skr::input::InputSystem* system, RC<input::InputMappingC
             event.global_delta = { delta.x, delta.y };
             sandbox->dispatch_event(&event);
         });
-        auto mapping    = system->create_mapping<skr::input::InputMapping_MouseAxis>(EMouseAxis::MOUSE_AXIS_XY);
+        auto mapping    = system->create_mapping<skr::input::InputMapping_MouseAxis>(input::EMouseAxis::MOUSE_AXIS_XY);
         mapping->action = action;
         ctx->add_mapping(mapping);
     }
@@ -102,7 +102,7 @@ void bind_pointer_event(skr::input::InputSystem* system, RC<input::InputMappingC
             event.scroll_delta = { delta.x, delta.y };
             sandbox->dispatch_event(&event);
         });
-        auto mapping    = system->create_mapping<skr::input::InputMapping_MouseAxis>(EMouseAxis::MOUSE_AXIS_WHEEL_XY);
+        auto mapping    = system->create_mapping<skr::input::InputMapping_MouseAxis>(input::EMouseAxis::MOUSE_AXIS_WHEEL_XY);
         mapping->action = action;
         ctx->add_mapping(mapping);
     }
