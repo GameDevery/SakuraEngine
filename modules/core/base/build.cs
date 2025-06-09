f2bd5e6cf709f76b231b85da5e0e1ec0f3febb4e
SaeruHikari <3045349520@qq.com>
2025-06-09 04:54:25 -0700

    fix compile on macosx

--- modules/core/base/build.cs
+++ modules/core/base/build.cs
@@ -8,7 +8,7 @@ public static class SkrBase
     {
         var Target = Engine.StaticComponent("SkrBase", "SkrCore")
             .EnableUnityBuild()
-            .Depend(Visibility.Public, "SkrProfile")
+            .Depend(Visibility.Public, "rtm", "SkrProfile")
             // TODO: MIGRATE COMPILE FLAGS FROM XMAKE
             // .Depend(Visibility.Public, "SkrCompileFlags")
             .IncludeDirs(Visibility.Public, "include")
