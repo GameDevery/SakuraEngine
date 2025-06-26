5d6c2a0e2631c72dc4207e4c3c09eb3a82aa7a76
sailing-innocent <colored_black@outlook.com>
2025-06-26 16:04:02 +0800

    temp: ozz read file and create skeleton

--- samples/animation/main.cpp
+++ samples/animation/main.cpp
@@ -7,11 +7,40 @@
 
 #include <iostream>
 #include <SkrAnim/ozz/skeleton.h>
+#include <string>
+#include <SkrAnim/ozz/base/io/archive.h>
 
 int main(int argc, char** argv)
 {
     std::cout << "Hello, SkrAnim!" << std::endl;
     ozz::animation::Skeleton skeleton;
     std::cout << "Skeleton created with " << skeleton.num_joints() << " joints." << std::endl;
+
+    // read ozz file
+    auto          filename = "D:/ws/data/assets/media/bin/ruby_skeleton.ozz";
+    ozz::io::File file(filename, "rb");
+
+    // Checks file status, which can be closed if filename is invalid.
+    if (!file.opened())
+    {
+        std::cout << "Cannot open file " << filename << "." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // deserialize
+    ozz::io::IArchive archive(&file);
+
+    // test archive
+    if (!archive.TestTag<ozz::animation::Skeleton>())
+    {
+        std::cout << "Archive doesn't contain the expected object type." << std::endl;
+        return EXIT_FAILURE;
+    }
+    // Create Runtime Skeleton
+    archive >> skeleton;
+
+    // print skeleton info
+    std::cout << "Skeleton loaded with " << skeleton.num_joints() << " joints." << std::endl;
+
     return 0;
 }
