fa9efcef1301c0b7e35e91e5cd98d498271c743b
SaeruHikari <3045349520@qq.com>
2025-07-31 13:37:29 +0800

    importer & asset creator

--- modules/tools/tool_core/include/SkrToolCore/cook_system/asset_meta.hpp
+++ modules/tools/tool_core/include/SkrToolCore/cook_system/asset_meta.hpp
@@ -12,7 +12,34 @@ namespace skd::asset
 sreflect_struct(guid = "8e78354d-acf9-40e2-8175-1ad40654360d" serde = @json)
 TOOL_CORE_API AssetMetadata
 {
+public:
+    using LoadFromJson = bool (*)(skr::archive::JsonReader* reader, skr::RC<AssetMetadata> object);
+    using StoreToJson = bool (*)(skr::archive::JsonWriter* writer, skr::RC<AssetMetadata> object);
     virtual ~AssetMetadata();
+
+    template <typename T>
+    inline static skr::RC<T> Create()
+    {
+        auto m = skr::RC<T>::New();
+        m->Load = +[](skr::archive::JsonReader* reader, skr::RC<AssetMetadata> object) {
+            auto derived = object.cast_static<T>();
+            return skr::json_read<T>(reader, *derived);
+        };
+        m->Store = +[](skr::archive::JsonWriter* writer, skr::RC<AssetMetadata> object) {
+            auto derived = object.cast_static<T>();
+            return skr::json_write<T>(writer, *derived);
+        };
+        return m;
+    }
+
+protected:
+    sattr(serde = @disable)
+    LoadFromJson Load;
+
+    sattr(serde = @disable)
+    StoreToJson Store;
+
+    AssetMetadata();
     SKR_RC_IMPL();
 };
 
@@ -69,7 +96,7 @@ inline skr::RC<T> AssetMetaFile::GetMetadata()
     }
     else if (!meta_content.is_empty())
     {
-        auto METADATA = skr::RC<T>::New();
+        auto METADATA = AssetMetadata::Create<T>();
         skr::archive::JsonReader reader(meta_content.view());
         skr::json_read(&reader, *METADATA);
         metadata = METADATA;
@@ -126,7 +153,7 @@ struct JsonSerde<skd::asset::AssetMetaFile>
             skr::json_read(&reader, v.cooker);
         // construct importer
         if (Parse(reader, u8"importer", false))
-            v.importer = skd::asset::GetImporterFactory()->LoadImporter(&reader);
+            v.importer = skd::asset::GetImporterRegistry()->LoadImporter(&reader);
         return true;
     }
 
@@ -144,7 +171,7 @@ struct JsonSerde<skd::asset::AssetMetaFile>
         if (v.importer)
         {
             w->Key(u8"importer");
-            skd::asset::GetImporterFactory()->StoreImporter(w, v.importer);
+            skd::asset::GetImporterRegistry()->StoreImporter(w, v.importer);
         }
         return true;
     }
--- modules/tools/tool_core/include/SkrToolCore/cook_system/importer.hpp
+++ modules/tools/tool_core/include/SkrToolCore/cook_system/importer.hpp
@@ -17,6 +17,11 @@ TOOL_CORE_API Importer
 {
 public:
     SKR_GENERATE_BODY();
+    
+    using CreateFN = skr::RC<Importer> (*)();
+    using LoadFromJson = bool (*)(skr::archive::JsonReader* reader, skr::RC<Importer> object);
+    using StoreToJson = bool (*)(skr::archive::JsonWriter* writer, skr::RC<Importer> object);
+
     static constexpr uint32_t kDevelopmentVersion = UINT32_MAX;
     static uint32_t Version() { return kDevelopmentVersion; }
 
@@ -25,18 +30,44 @@ public:
     virtual void Destroy(void*) = 0;
     inline skr::GUID GetType() const { return importer_type; }
 
+    template <typename T>
+    inline static skr::RC<T> Create()
+    {
+        auto i = skr::RC<T>::New();
+        i->Load = +[](skr::archive::JsonReader* reader, skr::RC<Importer> object) {
+            auto derived = object.cast_static<T>();
+            return skr::json_read<T>(reader, *derived);
+        };
+        i->Store = +[](skr::archive::JsonWriter* writer, skr::RC<Importer> object) {
+            auto derived = object.cast_static<T>();
+            return skr::json_write<T>(writer, *derived);
+        };
+        i->importer_type = skr::type_id_of<T>();
+        return i;
+    }
+
+protected:
     skr::GUID importer_type;
+
+    sattr(serde = @disable)
+    LoadFromJson Load = nullptr;
+    
+    sattr(serde = @disable)
+    StoreToJson Store = nullptr;
+
+    Importer();
     SKR_RC_IMPL();
 };
 
 struct TOOL_CORE_API ImporterTypeInfo
 {
-    skr::RC<Importer> (*Load)(skr::archive::JsonReader* reader);
-    void (*Store)(skr::archive::JsonWriter* writer, skr::RC<Importer> object);
+    Importer::CreateFN Create;
+    Importer::LoadFromJson Load;
+    Importer::StoreToJson Store;
     uint32_t (*Version)();
 };
 
-struct ImporterFactory
+struct ImporterRegistry
 {
     virtual skr::RC<Importer> LoadImporter(skr::archive::JsonReader* importer) = 0;
     virtual void StoreImporter(skr::archive::JsonWriter* writer, skr::RC<Importer> importer) = 0;
@@ -44,22 +75,25 @@ struct ImporterFactory
     virtual void RegisterImporter(skr::GUID type, ImporterTypeInfo info) = 0;
 };
 
-TOOL_CORE_API ImporterFactory* GetImporterFactory();
+TOOL_CORE_API ImporterRegistry* GetImporterRegistry();
 } // namespace skd::asset
 
 template <class T>
 void skd::asset::RegisterImporter(skr::GUID guid)
 {
-    auto registry = GetImporterFactory();
-    auto loader = +[](skr::archive::JsonReader* object) -> skr::RC<Importer> {
-        auto importer = skr::RC<T>::New();
-        skr::json_read(object, *importer);
-        return importer;
+    auto registry = GetImporterRegistry();
+    auto create = +[]() {
+        auto derived = Importer::Create<T>();
+        return derived.template cast_static<Importer>();
     };
-    auto store = +[](skr::archive::JsonWriter* writer, skr::RC<Importer> p) {
-        auto _this = (T*)p.get();
-        skr::json_write(writer, *_this);
+    auto loader = +[](skr::archive::JsonReader* reader, skr::RC<Importer> object) {
+        auto derived = object.cast_static<T>();
+        return skr::json_read<T>(reader, *derived);
     };
-    ImporterTypeInfo info{ loader, store, T::Version };
+    auto store = +[](skr::archive::JsonWriter* writer, skr::RC<Importer> object) {
+        auto derived = object.cast_static<T>();
+        return skr::json_write<T>(writer, *derived);
+    };
+    ImporterTypeInfo info{ create, loader, store, T::Version };
     registry->RegisterImporter(guid, info);
 }
